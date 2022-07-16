97ab8c00395488628be6bfec0ff82247b1049f99
Brandon <brandonperez@brandons-mbp.lan>
2022-07-16 15:22:58 -0400

    included options in webpacker to optimize images in and include them in the new bundles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,51 @@
 const path = require('path');
 const webpack  = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 module.exports ={
-    entry: './assets/js/script.js',
+    entry: {
+        app:"./assets/js/script.js",
+        events:"./assets/js/events.js",
+        schedule:"./assets/js/schedule.js",
+        tickets:"./assets/js/tickets.js"
+    },
+    module:{
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader:'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]'
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader:'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     output: {
-        path:path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery:'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outputs to an html file in the dist folder
+        })
     ],
     mode: 'development'
 }
