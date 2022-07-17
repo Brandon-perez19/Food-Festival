3e2d349f0ea7c7b2c619dbed9be7d878453291ac
Brandon <brandonperez@brandons-mbp.lan>
2022-07-16 21:02:00 -0400

    refractor webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,70 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const webpack  = require('webpack');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-module.exports ={
+const config = {
     entry: {
-        app:"./assets/js/script.js",
-        events:"./assets/js/events.js",
-        schedule:"./assets/js/schedule.js",
-        tickets:"./assets/js/tickets.js"
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
     },
-    module:{
+    output: {
+        path: path.join(__dirname + "/dist"),
+        filename: "[name].bundle.js"
+    },
+    module: {
         rules: [
             {
-                test: /\.jpg$/i,
+                test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
-                        loader:'file-loader',
+                        loader: 'file-loader',
                         options: {
                             esModule: false,
                             name(file) {
-                                return '[path][name].[ext]'
+                                return '[path][name].[ext]';
                             },
-                            publicPath: function(url) {
-                                return url.replace("../", "/assets/")
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
                             }
                         }
                     },
                     {
-                        loader:'image-webpack-loader'
+                        loader: 'image-webpack-loader'
                     }
                 ]
             }
         ]
     },
-    output: {
-        filename: "[name].bundle.js",
-        path: __dirname + "/dist",
-    },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
-            jQuery:'jquery'
+            jQuery: 'jquery'
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static", //the report outputs to an html file in the dist folder
+            analyzerMode: 'static'
         }),
         new WebpackPwaManifest({
-            name: "Food Event",
-            short_name: "Foodies",
-            description:" An app that allows you to view upcoming food events.",
-            start_url:"../index.html",
-            background_color: "#01579b",
-            theme_color:"#ffffff",
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
             fingerprints: false,
             inject: false,
-            icons: [{
-                src: path.resolve("assets/img/icons/icon-512x512.png"),
-                sizes:[96, 128, 192, 256, 384, 512],
-                destination: path.join("assets", "icons")
-            }]
+            icons: [
+                {
+                    src: path.resolve('assets/img/icons/icon-512x512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons')
+                }
+            ]
         })
     ],
     mode: 'development'
-}
+};
+
+module.exports = config;
